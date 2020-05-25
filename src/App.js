7dfe9064882de9fde9388d1273823c552c6c47c0
Dblackone <vollmannakarakiri0@gmail.com>
2020-05-25 23:39:07 +0100

    added tweets

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/header";
 import Facts from "./components/facts";
 import Footer from "./components/footer";
+import "./components/App.css";
 
 function App() {
   return (
--- src/components/facts.js
+++ src/components/facts.js
@@ -3,23 +3,96 @@ import "./App.css";
 
 function Facts(){
     return (
-        <div className="tweet-container">
+        <div className="tweets">
             <div className="tweet-container">
                 <div>
 
                 </div>
                 <div className="row">
                     <div className="col-2">
-                        <img className="" src="" alt=""/>
+                        <div className="img-container"><img className="tweet-img" src="https://res.cloudinary.com/start-ng/image/upload/c_crop,h_1500,w_1500/v1590272868/WhatsApp_Image_2020-05-23_at_23.08.15_bm1kr9.jpg" alt=""/></div>
+                        
                     </div>
                     <div className="col-10">
-                        <div></div>
+                        <div className="row ">
+                            <p className="tweet-name">A Fine Girl </p>
+                            <p className="username"> @goodgirl</p>
+                            <p className="time"> . 41m</p>
+                            <div></div>
+                        </div>
                         <div>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis exercitationem laudantium similique magni excepturi suscipit quisquam consequuntur molestias commodi quae eos, nisi veritatis quos eveniet cumque! Eaque a libero nulla?</p>
+                            <p className="tweet-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis exercitationem laudantium similique magni excepturi suscipit quisquam consequuntur molestias commodi quae eos, nisi veritatis quos eveniet cumque! Eaque a libero nulla?</p>
                         </div>
                     </div>
                 </div>
+            </div>
 
+            <div className="tweet-container">
+                <div>
+
+                </div>
+                <div className="row">
+                    <div className="col-2">
+                        <div className="img-container"><img className="tweet-img" src="https://res.cloudinary.com/start-ng/image/upload/c_crop,h_1500,w_1500/v1590272868/WhatsApp_Image_2020-05-23_at_23.08.15_bm1kr9.jpg" alt=""/></div>
+                        
+                    </div>
+                    <div className="col-10">
+                        <div className="row ">
+                            <p className="tweet-name">A Fine Girl </p>
+                            <p className="username"> @goodgirl</p>
+                            <p className="time"> . 41m</p>
+                            <div></div>
+                        </div>
+                        <div>
+                            <p className="tweet-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis exercitationem laudantium similique magni excepturi suscipit quisquam consequuntur molestias commodi quae eos, nisi veritatis quos eveniet cumque! Eaque a libero nulla?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="tweet-container">
+                <div>
+
+                </div>
+                <div className="row">
+                    <div className="col-2">
+                        <div className="img-container"><img className="tweet-img" src="https://res.cloudinary.com/start-ng/image/upload/c_crop,h_1500,w_1500/v1590272868/WhatsApp_Image_2020-05-23_at_23.08.15_bm1kr9.jpg" alt=""/></div>
+                        
+                    </div>
+                    <div className="col-10">
+                        <div className="row ">
+                            <p className="tweet-name">A Fine Girl </p>
+                            <p className="username"> @goodgirl</p>
+                            <p className="time"> . 41m</p>
+                            <div></div>
+                        </div>
+                        <div>
+                            <p className="tweet-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis exercitationem laudantium similique magni excepturi suscipit quisquam consequuntur molestias commodi quae eos, nisi veritatis quos eveniet cumque! Eaque a libero nulla?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="tweet-container">
+                <div>
+
+                </div>
+                <div className="row">
+                    <div className="col-2">
+                        <div className="img-container"><img className="tweet-img" src="https://res.cloudinary.com/start-ng/image/upload/c_crop,h_1500,w_1500/v1590272868/WhatsApp_Image_2020-05-23_at_23.08.15_bm1kr9.jpg" alt=""/></div>
+                        
+                    </div>
+                    <div className="col-10">
+                        <div className="row ">
+                            <p className="tweet-name">A Fine Girl </p>
+                            <p className="username"> @goodgirl</p>
+                            <p className="time"> . 41m</p>
+                            <div></div>
+                        </div>
+                        <div>
+                            <p className="tweet-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis exercitationem laudantium similique magni excepturi suscipit quisquam consequuntur molestias commodi quae eos, nisi veritatis quos eveniet cumque! Eaque a libero nulla?</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
