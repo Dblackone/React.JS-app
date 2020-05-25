fae8dbfcaa24536c371b9473368dae27740970b2
Dblackone <vollmannakarakiri0@gmail.com>
2020-05-25 22:27:11 +0100

    comit

--- src/components/facts.js
+++ src/components/facts.js
@@ -9,8 +9,15 @@ function Facts(){
 
                 </div>
                 <div className="row">
-                    <div className="col-2"></div>
-                    <div className="col-10"></div>
+                    <div className="col-2">
+                        <img className="" src="" alt=""/>
+                    </div>
+                    <div className="col-10">
+                        <div></div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis exercitationem laudantium similique magni excepturi suscipit quisquam consequuntur molestias commodi quae eos, nisi veritatis quos eveniet cumque! Eaque a libero nulla?</p>
+                        </div>
+                    </div>
                 </div>
 
             </div>
